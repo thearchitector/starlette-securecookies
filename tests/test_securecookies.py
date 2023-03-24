f5278c089805ea6b1497a1fe253d23a31521ab7b
Elias Gabriel <me@eliasfgabriel.com>
2023-03-24 00:31:23 -0400

    fix: tests and deprecation warnings

--- tests/test_securecookies.py
+++ tests/test_securecookies.py
@@ -1,38 +1,42 @@
 import pytest
+
 from securecookies import BadArgumentError
 
 
-def test_overcontrained(client_factory):
+def test_overconstrained(client_factory):
     with pytest.raises(BadArgumentError, match="doesn't make sense"):
-        client_factory(included_cookies=["some"], excluded_cookies=["none"])
+        client = client_factory(included_cookies=["some"], excluded_cookies=["none"])
+        client.get("/get")
 
 
 def test_bad_samesite(client_factory):
-    client_factory(cookie_samesite="strict")
+    client = client_factory(cookie_samesite="strict")
+    client.get("/get")
 
     with pytest.raises(BadArgumentError, match="SameSite"):
-        client_factory(cookie_samesite="invalid")
+        client = client_factory(cookie_samesite="invalid")
+        client.get("/get")
 
 
-def test_outgoing(client, fernet):
+def test_outgoing(client_factory, fernet):
     # api write ginger-molasses, client needs to decrypt
-    response = client.get("/get")
-
+    response = client_factory().get("/get")
     assert response.status_code == 200
-    cookie = [*response.cookies][0]
-    assert fernet.decrypt(cookie.value.encode()).decode() == "ginger-molasses"
-    assert not cookie.secure
+
+    cookie = response.cookies["type"]
+    assert fernet.decrypt(cookie.encode()).decode() == "ginger-molasses"
+    assert not next(iter(response.cookies.jar)).secure
 
 
 def test_outgoing_complex(client_factory, fernet):
     # api write ginger-molasses overwrites secure=True, client needs to decrypt
     client = client_factory(cookie_secure=True)
     response = client.get("/get")
-
     assert response.status_code == 200
-    cookie = [*response.cookies][0]
-    assert fernet.decrypt(cookie.value.encode()).decode() == "ginger-molasses"
-    assert cookie.secure
+
+    cookie = response.cookies["type"]
+    assert fernet.decrypt(cookie.encode()).decode() == "ginger-molasses"
+    assert next(iter(response.cookies.jar)).secure
 
 
 def test_outgoing_included(client_factory, fernet):
@@ -41,67 +45,65 @@ def test_outgoing_included(client_factory, fernet):
     response = client.get("/getm")
 
     assert response.status_code == 200
-    cookies = [*response.cookies]
-    print(cookies)
-    assert cookies[0].name == "anothertype"
-    assert cookies[1].name == "type"
-    assert cookies[0].value == "chocolate-chip"
-    assert fernet.decrypt(cookies[1].value.encode()).decode() == "ginger-molasses"
+    assert (
+        fernet.decrypt(response.cookies["type"].encode()).decode() == "ginger-molasses"
+    )
+    assert response.cookies["anothertype"] == "chocolate-chip"
 
 
 def test_outgoing_excluded(client_factory, fernet):
     # type should not be encrypted
     client = client_factory(excluded_cookies=["type"])
     response = client.get("/getm")
-
     assert response.status_code == 200
-    cookies = [*response.cookies]
-    assert cookies[0].name == "anothertype"
-    assert cookies[1].name == "type"
-    assert fernet.decrypt(cookies[0].value.encode()).decode() == "chocolate-chip"
-    assert cookies[1].value == "ginger-molasses"
+
+    cookie = response.cookies["anothertype"]
+    assert fernet.decrypt(cookie.encode()).decode() == "chocolate-chip"
+    assert response.cookies["type"] == "ginger-molasses"
 
 
-def test_incoming(client, mock_cookies, fernet):
+def test_incoming(client_factory, mock_cookies, fernet):
     # client passes encrypted, should validate if decrypted in middleware
-    response = client.get("/validate", cookies=mock_cookies)
-
+    client = client_factory(cookies=mock_cookies)
+    response = client.get("/validate")
     assert response.status_code == 200
-    assert (
-        fernet.decrypt([*response.cookies][0].value.encode()).decode()
-        == "iateyoursecret"
-    )
+
+    cookie = response.cookies["topsecret"]
+    assert fernet.decrypt(cookie.encode()).decode() == "iateyoursecret"
 
 
 def test_incoming_included(client_factory, mock_cookies, fernet):
     # don't discard insecure, so should be able to read value
     client = client_factory(
-        included_cookies=["topsecret", "othercookie"], discard_invalid=False
+        cookies=mock_cookies,
+        included_cookies=["topsecret", "othercookie"],
+        discard_invalid=False,
     )
-    response = client.get("/validate", cookies=mock_cookies)
-
+    response = client.get("/validate")
     assert response.status_code == 200
-    assert (
-        fernet.decrypt([*response.cookies][0].value.encode()).decode()
-        == "notreallyasecret"
-    )
+
+    topsecret = response.cookies["topsecret"]
+    othercookie = response.cookies["othercookie"]
+    assert fernet.decrypt(topsecret.encode()).decode() == "iateyoursecret"
+    assert fernet.decrypt(othercookie.encode()).decode() == "notreallyasecret"
 
 
-def test_incoming_included_fail(client_factory, mock_cookies, fernet):
+def test_incoming_included_discarded(client_factory, mock_cookies, fernet):
     # do discard insecure, so shouldn't be able to read value
-    client = client_factory(included_cookies=["topsecret", "othercookie"])
-    response = client.get("/validate", cookies=mock_cookies)
-
-    assert response.status_code == 200
-    assert (
-        fernet.decrypt([*response.cookies][0].value.encode()).decode()
-        == "iateyoursecret"
+    client = client_factory(
+        cookies=mock_cookies, included_cookies=["topsecret", "othercookie"]
     )
+    response = client.get("/validate")
+    assert response.status_code == 200
+
+    topsecret = response.cookies["topsecret"]
+    assert fernet.decrypt(topsecret.encode()).decode() == "iateyoursecret"
+    assert "othercookie" not in response.cookies
 
 
 def test_incoming_excluded(client_factory, mock_cookies, fernet):
     # excluding secure cookie means validation should fail
-    client = client_factory(excluded_cookies=["topsecret"])
+    client = client_factory(cookies=mock_cookies, excluded_cookies=["topsecret"])
 
     with pytest.raises(AssertionError, match="thisisasecretcookie"):
-        client.get("/validate", cookies=mock_cookies)
+        client.get("/validate")
