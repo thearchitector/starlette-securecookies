8fa36e7df904c2427bd273e981c6ab31c1d5d0b4
Elias Gabriel <me@eliasfgabriel.com>
2023-04-25 01:32:42 -0400

    docs: add csrf extra information to docs
    fix: pull in pdoc mermaid support

--- securecookies/extras/__init__.py
+++ securecookies/extras/__init__.py
@@ -4,8 +4,11 @@
 starlette-securecookies also ships with some common middleware and tools modified and
 extended to work with secure cookies.
 
-- **`csrf.SecureCSRFMiddleware`**: Adds compatibility to CSRFMiddleware provided by the
-[starlette_csrf](https://github.com/frankie567/starlette-csrf) package. You can add
-starlette_csrf by using the `starlette-securecookies[csrf]` extra or installing it
-manually.
+- **`csrf.SecureCSRFMiddleware`**: Adds compatibility to the CSRF middleware provided
+by [starlette_csrf](https://github.com/frankie567/starlette-csrf). To use it, simply
+add it to your list of middleware (keep in mind the ordering). If you don't want to
+specify `starlette_csrf` as a direct dependency, you can also install it through the
+`[csrf]` package extra.
 """
+
+__all__ = []  # type: ignore
